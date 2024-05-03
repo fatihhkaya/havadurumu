110d6784e0b179b598ae8c32edff2365fb033850
Fatih Kaya <fatih.55kaya.1@gmail.com>
2024-05-03 16:35:22 +0300

    Initial Commit

---
+++ havadurumu/havadurumuApp.swift
@@ -0,0 +1,17 @@
+//
+//  havadurumuApp.swift
+//  havadurumu
+//
+//  Created by Fatih on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct havadurumuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
